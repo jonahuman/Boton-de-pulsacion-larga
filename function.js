aa87ebcd6d41370c2f6584a00d7c8eb4387354ee
flerchai <97056884+flerkendroid@users.noreply.github.com>
2023-05-15 20:58:39 -0400

    He escrito código en function.js

--- function.js
+++ function.js
@@ -1,1 +1,11 @@
+// hecho por flerkendroid
 
+function myLight() {
+$('h1').css({'filter':'grayscale(0%)'});
+$("h2").attr("onclick", "myNight()");
+    }
+
+function myNight() {
+$('h1').css({'filter':'grayscale(100%)'});
+$("h2").attr("onclick", "myLight()");
+    }
